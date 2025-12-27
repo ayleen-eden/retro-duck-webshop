8045d6c8f9677c5738d328be35b3042afc21ffcf
Ljonja <60048962+theljonja@users.noreply.github.com>
2025-12-27 16:59:13 +0100

    feat: implement product repository and service logic

---
+++ swa-skeleton-main/src/main/java/at/qe/skeleton/services/ProductService.java
@@ -0,0 +1,38 @@
+package at.qe.skeleton.services;
+
+import at.qe.skeleton.model.Product;
+import at.qe.skeleton.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+@Scope("application")
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    // ===== READ =====
+
+    public Collection<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    // ===== WRITE =====
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
