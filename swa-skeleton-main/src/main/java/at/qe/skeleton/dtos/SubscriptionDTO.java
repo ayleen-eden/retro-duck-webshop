91dcbcdefd81bbb49940419790210a9d15dbefff
Jakob Oberhofer <j.oberhofer@student.uibk.ac.at>
2026-01-11 18:13:09 +0100

    Revert "Merge branch 'feature/subscriptions' into 'main'"
    This reverts merge request !13

--- swa-skeleton-main/src/main/java/at/qe/skeleton/dtos/SubscriptionDTO.java
+++
@@ -1,7 +0,0 @@
-package at.qe.skeleton.dtos;
-
-public record SubscriptionDTO(
-    Long id,
-    Long userId,
-    Long productId
-) {}
--- swa-skeleton-main/src/main/java/at/qe/skeleton/model/ProductCategory.java
+++ swa-skeleton-main/src/main/java/at/qe/skeleton/model/ProductCategory.java
@@ -1,14 +1,6 @@
 package at.qe.skeleton.model;
 
 public enum ProductCategory {
-    N64,
-    GCN,
-    GB,
-    WII,
-    SWITCH_2,
-    PS3,
-    PC,
-    NES,
-    SNES,
-    WII_U
+    // TODO: Find category-names
+    NES, SNES
 }
