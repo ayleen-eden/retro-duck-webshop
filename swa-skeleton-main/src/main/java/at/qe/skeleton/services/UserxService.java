106d5d47859e995981af19e7dac6937f591b5a47
Jakob Oberhofer <j.oberhofer@student.uibk.ac.at>
2026-01-24 13:59:34 +0100

    UserxRole gets automatically set to CUSTOMER when signing up

--- swa-skeleton-main/src/main/java/at/qe/skeleton/services/UserxService.java
+++ swa-skeleton-main/src/main/java/at/qe/skeleton/services/UserxService.java
@@ -3,6 +3,7 @@ package at.qe.skeleton.services;
 import at.qe.skeleton.dtos.UserProfileUpdateDTO;
 import at.qe.skeleton.exceptions.UsernameDuplicateException;
 import at.qe.skeleton.model.Userx;
+import at.qe.skeleton.model.UserxRole;
 import at.qe.skeleton.repositories.UserxRepository;
 import jakarta.annotation.security.PermitAll;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -71,6 +72,9 @@ public class UserxService implements UserDetailsService {
         if (user.isNew()) {
             if (userRepository.existsByUsername(user.getUsername())) {
                 throw new UsernameDuplicateException("Username " + user.getUsername() + " not available");
+            }
+            if (user.getRoles() == null || user.getRoles().isEmpty()) {
+                user.setRoles(java.util.Set.of(UserxRole.CUSTOMER));
             }
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             Userx authUser = authenticatedUserService.getAuthenticatedUser();
