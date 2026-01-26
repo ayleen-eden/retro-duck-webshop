139fe86d79556a08c108c17012f426d8a08e4777
Ayleen Edenhauser <ayleen.edenhauser@student.uibk.ac.at>
2026-01-26 12:41:09 +0100

    update: mail service uses log instead of sout

--- swa-skeleton-main/src/main/java/at/qe/skeleton/services/StubMailService.java
+++ swa-skeleton-main/src/main/java/at/qe/skeleton/services/StubMailService.java
@@ -1,17 +1,22 @@
 package at.qe.skeleton.services;
 
 import at.qe.skeleton.model.MailCategory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StubMailService {
 
+    private Logger log = LoggerFactory.getLogger(StubMailService.class);
+
     public void sendMail(String to, String subject, String body, MailCategory category) {
-        System.out.println("===== STUB MAIL OF CATEGORY " + category.toString() + " =====");
-        System.out.println("To: " + to);
-        System.out.println("Subject: " + subject);
-        System.out.println("Body: \n" + body);
-        System.out.println("===================================================");
+        log.info("===== STUB MAIL OF CATEGORY " + category.toString() + " =====");
+        log.info("To: " + to);
+        log.info("Subject: " + subject);
+        log.info("Body: \n" + body);
+        log.info("===================================================");
     }
 
 }
