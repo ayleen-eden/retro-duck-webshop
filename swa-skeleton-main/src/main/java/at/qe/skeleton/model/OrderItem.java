55bd32b1628014cc369907b707daa14ade4367df
Jakob Oberhofer <j.oberhofer@student.uibk.ac.at>
2026-01-30 17:52:34 +0100

    refactor: remove unused serialVersionUID from order models

--- swa-skeleton-main/src/main/java/at/qe/skeleton/model/OrderItem.java
+++ swa-skeleton-main/src/main/java/at/qe/skeleton/model/OrderItem.java
@@ -6,8 +6,6 @@ import java.io.Serializable;
 @Entity
 public class OrderItem implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
